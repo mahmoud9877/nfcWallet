aca311af43f98b155a80f91a170b60c2a5431ad1
mahmoud9877 <mohmoudali9877@gmail.com>
2024-12-18 20:37:29 +0200

    home page

--- src/app.router.js
+++ src/app.router.js
@@ -3,10 +3,12 @@ import connectDB from "../DB/connection.js";
 import auth from "../src/module/auth/auth.router.js";
 import profileUser from "./module/profile/profile.router.js";
 import { globalErrorHandling } from "./utils/errorHandling.js";
-
 const initApp = (app, express) => {
   app.use(cors());
   app.use(express.json());
+
+  app.get("/", (req, res) => res.json({ message: "home" }));
+
   app.use("/profile", profileUser);
   app.use("/auth", auth);
   app.use(globalErrorHandling);
